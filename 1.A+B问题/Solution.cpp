9940b85c77892dcce616ad393290fbe905cf6b88
GondorFu <fujinbin0721@126.com>
2017-05-19 16:50:54 -0400

    md

---
+++ 1.A+B问题/Solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /*
+     * @param a: The first integer
+     * @param b: The second integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        // write your code here, try to do it without arithmetic operators.
+        return (!b ? a : aplusb(a ^ b, (a & b) << 1));
+    }
+};
